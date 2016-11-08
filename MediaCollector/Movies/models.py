f1eb803c1bc58c45326d825d05f1995eb82a7ea2
Alex Hurd <odty101@gmail.com>
2016-11-07 21:38:36 -0800

    Now with more fields (working on DB design)

--- MediaCollector/Movies/models.py
+++ MediaCollector/Movies/models.py
@@ -2,9 +2,22 @@ from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, unique_for_year="release_date")
     release_date = models.DateField('Release Date')
-    runtime = models.IntegerField()
+    runtime = models.PositiveSmallIntegerField()
+
+    FORMAT_CHOICES = (
+        ('BRD', 'BRD'),
+        ('DVD', 'DVD'),
+        ('VHS', 'VHS'),
+    )
+    format = models.CharField(
+        max_length = 3,
+        choices = FORMAT_CHOICES,
+        default = 'BRD'
+    )
+
+    datetime_added = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self):
         return self.title + " (" + str(self.release_date.year) + ")"
--- MediaCollector/Movies/views.py
+++ MediaCollector/Movies/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
+from .models import Movie
+
 
 def index(request):
+    latest_movie_list = Movie.objects.order_by('-datetime_added')[:5]
+    output = ', '.join([movie.title for movie in latest_movie_list])
+    return HttpResponse(output)
